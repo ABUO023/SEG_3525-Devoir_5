8bc9e58247bc8dcab64b61829382c77c46ed5c04
Aditya Baindur <adityabaindur@icloud.com>
2026-07-18 17:53:49 -0400

    added auto redirects to important links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/ABUO023/SEG_3525-Devoir_5",
+        permanent: true,
+      },
+      {
+        source: "/figma",
+        destination:
+          "https://www.figma.com/design/5JhqwhrtXKBQmKPNKeiuNn/Untitled?node-id=0-1&t=m0mtzu6eAHQJrjMC-1",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
